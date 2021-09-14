707be6c98b36fe40823efa8fe3dcba7370616503
daniel_lerma_qtDev <daniel.lerma@qualytech.club>
2021-09-14 08:23:00 -0500

    platillo done

--- platillo.py
+++ platillo.py
@@ -1,17 +1,17 @@
 from MenuItem import MenuItem
 
 class Platillo(MenuItem): 
-    def __init__(self, precio: float, nombre: str, size: float) -> None:
+    def __init__(self, precio: float, nombre: str, tamaño: float) -> None:
         super().__init__()
         self.precio = precio
         self.nombre = nombre
-        self.size = size
+        self.tamaño = tamaño
 
     def servir(self):
         return f'El platillo {self.nombre} se está sirviendo'
 
     def getPrecio(self):
-        pass
+        return self.precio * self.tamaño
 
     def __str__(self) -> str:
         return f'{super().__str__()}{self.nombre} - {self.precio}'
