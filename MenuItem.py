bd95a303316e2399d73d4c4cac1f01ee558a3791
Rodolfo <rluthen@gmail.com>
2021-09-14 08:05:01 -0500

    MenuItem

---
+++ MenuItem.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class MenuItem(ABC):
+    @abstractmethod
+    def servir(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_precio(self):
+        raise NotImplementedError
+
