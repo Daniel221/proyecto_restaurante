02e90b59750aa43af792537e0061d9b4a6ba5cb7
Daniel Hernandez <daniel66670@hotmail.com>
2021-09-14 08:53:25 -0500

    finished details :v

--- Persona.py
+++ Persona.py
@@ -1,16 +1,17 @@
 from MenuItem import MenuItem
 
 class Persona:
-  def __init__(self):
+  def __init__(self, nombre:str):
     self._orden = []
     self._total = 0.0
+    self.nombre = nombre
 
   def addItem(self, item:MenuItem) -> float:
     self._orden.append(item)
     self._total += item.get_precio()
 
   def __str__(self):
-    salida = ''
+    salida = f'cuenta de {self.nombre}:\n'
     for item in self._orden:
       salida += f'{item}\n'
     return salida
